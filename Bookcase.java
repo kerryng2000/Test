1ecbe3486a749550bdc36f52baf1566831217b7d
Kerry Ng <kerryng00@gmail.com>
2018-03-28 21:34:56 -0700

    C-Test
    For PLTW and RedditDailyProgramming

---
+++ Bookcase.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Bookcase
+{
+	public static void main(String[] args)
+	{
+	int book[] = new int[1000];
+		int width[] = new int[1000];
+		Scanner scan = new Scanner(System.in);
+		for(int i < 0; i < book.length; i++)
+		{
+			book[i] = scan.nextint();
+		}
+	}
+}
+	
